880db05b7f6e920991c5abb993cd13b8b8758e3e
Guido Vranken <guidovranken@gmail.com>
2017-12-18 15:34:30 +0100

    instrument_vm.sh: instrument entire core/ directory

--- instrument_vm.sh
+++ instrument_vm.sh
@@ -1,5 +1,3 @@
 GO_INSTRUMENT=/media/jhg/5c980a66-cf42-464d-878a-85016937237c/gopkg/src/github.com/guidovranken/go-coverage-instrumentation/instrument/main
-cd core
-rm -rf vm_instrumented
-mkdir vm_instrumented
-$GO_INSTRUMENT vm vm_instrumented
+mkdir core_instrumented
+$GO_INSTRUMENT core core_instrumented
