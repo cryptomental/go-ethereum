2d0a713bd95edb2e1cf8e910788612792b270a5a
Cryptomental <cryptomental.com@gmail.com>
2019-01-05 19:58:40 +0100

    core/vm/interpreter: Fix v1.8.20 merge error.

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -259,21 +259,12 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 				return nil, errGasUintOverflow
 			}
 		}
-
-		if !in.cfg.DisableGasMetering {
-			// consume the gas and return an error if not enough gas is available.
-			// cost is explicitly set so that the capture state defer method cas get the proper cost
-			cost, err = operation.gasCost(in.gasTable, in.evm, contract, stack, mem, memorySize)
-			if err != nil || !contract.UseGas(cost) {
-                
-				return nil, ErrOutOfGas
-			}
 		// consume the gas and return an error if not enough gas is available.
 		// cost is explicitly set so that the capture state defer method can get the proper cost
 		cost, err = operation.gasCost(in.gasTable, in.evm, contract, stack, mem, memorySize)
 		if err != nil || !contract.UseGas(cost) {
 			if in.cfg.Debug && in.evm.depth > 1 {
-				 in.cfg.Tracer.CaptureState(in.evm, pc, op, gasCopy, cost, mem, stackCopy, contract, in.evm.depth, err)
+				 in.cfg.Tracer.CaptureState(in.evm, pc, op, gasCopy, cost, mem, stack, contract, in.evm.depth, err)
 			}
 			return nil, ErrOutOfGas
 		}
